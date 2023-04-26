5c92bf7b2acc897c11f721058280f094f2fa94b0
Jeymsson Nogueira <jeymsson@gmail.com>
2023-04-26 12:57:50 -0300

    feat: add new component in project

---
+++ code/src/components/ClickCounter.vue
@@ -0,0 +1,16 @@
+<template>
+	<button @click="soma">Click {{ count }}</button>
+</template>
+<script>
+	import { ref } from 'vue'
+	export default {
+		setup() {
+			const count = ref(0);
+			const soma = () => { count.value++; };
+			return {
+				count,
+				soma,
+			};
+		}
+	}
+</script>
